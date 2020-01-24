36332f154837ecce69bea2dff43c7150f9e8e6f8
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    extra: Use Pixel charger animation
    Author: Arseniy Graur <argraur@gmail.com>
    Date:   Fri Jan 24 14:08:14 2020 +0000
    
        extra: Use Pixel charger animation
    
        Signed-off-by: Arseniy Graur <argraur@gmail.com>
    
    Author: Dyneteve <dyneteve@hentaios.com>
    Date:   Fri Oct 22 19:08:12 2021 +0200
    
        charger: Import latest Pixel offline charging animation
    
        Change-Id: I972c6e3e334754240c9098af21863f042ef38e47
    
    Change-Id: I64d3947bafaccda2472e621d86284df9fa22d131

--- product.mk
+++ product.mk
@@ -1,3 +1,7 @@
+# Charger
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,$(TARGET_COPY_OUT_PRODUCT)/etc/res)
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksOverlayExtra
