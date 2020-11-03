98a1e188c0e1e05307ffe7aa5c3bb62e577f5fd1
Danny Lin <danny@kdrag0n.dev>
2020-11-02 22:45:00 -0800

    ThemePicker: Use generic default themes stub
    Change-Id: Iad63b3d8c2ea42812f26344111890fbef2274d2d

--- product.mk
+++ product.mk
@@ -80,7 +80,8 @@ $(call inherit-product-if-exists, device/*/$(TARGET_PRODUCT)-kernel/kernel.mk)
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayExtra \
     FrameworksOverlayExtra \
-    SystemUIOverlayExtra
+    SystemUIOverlayExtra \
+    ThemePickerOverlayExtra
 
 # Packages
 PRODUCT_PACKAGES += \
