0a5c14a46117133390c963a1b2573ab160b258dd
SamarV-121 <samarvispute121@pm.me>
2021-12-21 12:52:32 +0530

    extra: Compile ih8sn
     * https://github.com/luk1337/ih8sn
    
    Change-Id: Id5852723769436d4ef272f2f92aa43d3f5992fc1

--- product.mk
+++ product.mk
@@ -2,6 +2,13 @@
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,$(TARGET_COPY_OUT_PRODUCT)/etc/res)
 
+# ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksOverlayExtra
